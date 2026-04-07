f481a349cd60deaa69c4712d601412619b6f79f6
Evanio Tech <evanioshark@gmail.com>
2026-04-07 17:36:05 -0300

    Usando console.log e camelCase

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ console.log(cliente)
 // Constantes não podem ser alteradas
 const sexo = "Masculino"
 console.log(sexo)
+
+// console.log é uma função que imprime o que está entre parênteses no console do navegador ou do terminal.
+
+cliente = "Maria"
+console.log(" O Nome da nossa segunda cliente é: " + cliente)
+
+// camelCase é onde a primeira letra da primeira palavra é minúscula e a primeira letra de cada palavra subsequente é maiúscula.
+
+let nomeDoCliente = "João"
+console.log(nomeDoCliente)
