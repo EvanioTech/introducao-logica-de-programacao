d80a376eb2e115b4214660e3ad16a3a0f2b0b18a
Evanio Tech <evanioshark@gmail.com>
2026-04-07 17:30:19 -0300

    Usando constantes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("introdução à Lógica de Programação")
 
-// Variáveis
+// Variáveis podem ser alteradas
 
 let cliente = "Matias"
 console.log(cliente)
+
+// Constantes não podem ser alteradas
+const sexo = "Masculino"
+console.log(sexo)
